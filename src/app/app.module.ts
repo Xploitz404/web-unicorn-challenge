905c79264b656527e5597d9de883687392978aad
Oscar Lara <oscar.lara@2bcore.com>
2019-07-13 18:10:07 -0500

    create (landing): Se crean módulo y componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShellModule } from './core/shell/shell.module';
+import { LandingModule } from './components/landing/landing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ShellModule
+    ShellModule,
+    LandingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
