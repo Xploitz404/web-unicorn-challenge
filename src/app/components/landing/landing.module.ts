d8df00b8613310a2e937035177ef913286dd7d1c
Oscar Lara <oscar.lara@2bcore.com>
2019-07-13 18:22:38 -0500

    update (): Se organizan componentes y se agrega ruta principal

--- src/app/components/landing/landing.module.ts
+++ src/app/components/landing/landing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingComponent } from './landing.component';
+import { ShellModule } from '../../core/shell/shell.module';
 
 @NgModule({
   declarations: [
     LandingComponent
   ],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    ShellModule
+  ],
   exports: [
     LandingComponent
   ],
