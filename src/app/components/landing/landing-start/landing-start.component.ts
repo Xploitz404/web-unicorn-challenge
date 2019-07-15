d6f5db3af5b7ef47ce46ccad88f4d046bd0f065a
Oscar Lara <oscar.lara@2bcore.com>
2019-07-14 20:08:31 -0500

    feat (landing-start): Se concluyen estructura y estilos desktop

--- src/app/components/landing/landing-start/landing-start.component.ts
+++ src/app/components/landing/landing-start/landing-start.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CARDS } from '../../../constants/start-cards/start-cards.const';
+
 @Component({
   selector: 'unicorn-landing-start',
   templateUrl: './landing-start.component.html',
   styleUrls: ['./landing-start.component.sass']
 })
 export class LandingStartComponent implements OnInit {
+
+  cards = CARDS;
 
   constructor() { }
 
---
+++ src/app/constants/start-cards/start-cards.const.ts
@@ -0,0 +1,19 @@
+/**
+ * Constante que almacena la información relacionada con las cards del inicio
+ * @const
+ */
+
+export const CARDS = [
+  {
+    class: 'card-1',
+    icon: '/assets/icons/development.png',
+    text: 'HTML + CSS + Responsive Design',
+    title: 'Desarrollo Web'
+  },
+  {
+    class: 'card-2',
+    icon: '/assets/icons/design.png',
+    text: 'UI/UX + Adobe XD/Figma + Zeplin',
+    title: 'Diseño Web'
+  }
+];
