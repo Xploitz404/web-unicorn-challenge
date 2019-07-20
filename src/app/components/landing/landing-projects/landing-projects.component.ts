ae891604ed1da8ccdad4031f9e6c0e5a1e6bca21
Oscar Lara <oscar.lara@2bcore.com>
2019-07-20 16:37:04 -0500

    create (landing-projects): Se crea componente

---
+++ src/app/components/landing/landing-projects/landing-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'unicorn-landing-projects',
+  templateUrl: './landing-projects.component.html',
+  styleUrls: ['./landing-projects.component.sass']
+})
+export class LandingProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/landing/landing.module.ts
+++ src/app/components/landing/landing.module.ts
@@ -5,12 +5,14 @@ import { LandingComponent } from './landing.component';
 import { ShellModule } from '../../core/shell/shell.module';
 import { LandingStartComponent } from './landing-start/landing-start.component';
 import { LandingAboutMeComponent } from './landing-about-me/landing-about-me.component';
+import { LandingProjectsComponent } from './landing-projects/landing-projects.component';
 
 @NgModule({
   declarations: [
     LandingComponent,
     LandingStartComponent,
-    LandingAboutMeComponent
+    LandingAboutMeComponent,
+    LandingProjectsComponent
   ],
   imports: [
     CommonModule,
