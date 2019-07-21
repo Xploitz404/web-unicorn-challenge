2dad97f8fffe4dd11235a4acea697f8f53a8a162
Oscar Lara <oscar.lara@2bcore.com>
2019-07-21 15:34:52 -0500

    create (footer): Se crea componente

--- src/app/core/shell/shell.module.ts
+++ src/app/core/shell/shell.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   imports: [ CommonModule ],
-  exports: [ HeaderComponent, SidebarComponent],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   providers: [],
 })
 export class ShellModule {}
